408cbc6d3efe93140f033ce233d98fa864d55f44
Chris B <cbega1227@gmail.com>
2022-10-29 23:37:22 -0600

    Finished building function to display password.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,96 +1,43 @@
+// Created variables for numbers, uppercase, lowercase, and special characters
 var numbersArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var upperArr = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-var lowerArr = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
 var specialArr = ["!", "@", "#", "$", "*", "%"];
+var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Created a varilbe that will store an array for when the user inputs their password criteria.
 const selectedCriteria = [];
 
-// Collecting password criteria function
+// Created a fuction that will store the users password criteria and verify some condtions.
 function collectingCriteria() {
-  // Created var to collect the passwordLength input from user.
-  var passwordLength = prompt(
-    "Choose your password lenght (min: 8 || max: 128): "
-  );
+  // Created a varaible to store the users input for passwordLength.
+  var passwordLength = prompt("Choose your password lenght (min: 8 || max: 128): ");
+
   // Created an if statment to verify passwordLength input is no less than 8 or no greater than 128
   if (passwordLength < 8 || passwordLength > 128) {
     window.alert("Invalid password length.");
-    // Prompt user to retry if true recall collectingCriteria || EXIT
+    // Created a variable to collect t/f.
     var reStart = confirm("Would you like to retry?");
+    // Created an if statment to determine if the user wants to reattempt to gen. the password or to exit.
     if (reStart === true) {
       collectingCriteria();
       return;
     }
   }
-  // Collecting rest of criteria from user using true/false
+
+  // Created variables to store the rest of the criteria from the user using true/false.
   var confirmNumbers = confirm("Would you like numbers?");
   var confirmUpperCase = confirm("Would you like upper case letters?");
   var confirmLowerCase = confirm("Would you like lower case letters?");
   var confirmSpecialChar = confirm("Would you like special characters?");
 
-  // Verifying user selected atleast one criteria
-  if (
-    confirmNumbers === false &&
-    confirmUpperCase === false &&
-    confirmLowerCase === false &&
-    confirmSpecialChar === false
-  ) {
-    var verification = window.confirm(
-      "You've selected ZERO character settings. Try again?"
-    );
+  // Created an if statement to verifying if the user selected atleast a minimum of one criteria.
+  if (confirmNumbers === false && confirmUpperCase === false && confirmLowerCase === false && confirmSpecialChar === false) {
+    // Created a variable to store t/f.
+    var verification = window.confirm("You've selected ZERO character settings. Try again?");
+    // Created an if statment to determine if the user wants to reattempt to gen. the password or to exit.
     if (verification === true) {
       collectingCriteria();
       return;
@@ -98,54 +45,55 @@ function collectingCriteria() {
       window.alert("Okay, goodbye.");
     }
   }
-  // Sorting Criteria if true then push to selectedCriteria Array
 
-  // Set an empty array for selected criteria
+  //Created a series of if statements to determine what criteria the user selected.
+  //Create an if statment to see if the criteria is true
   if (confirmNumbers === true) {
+    // if true then .push the criteria type to selectedCriteria array
     selectedCriteria.push(numbersArr);
-    console.log(selectedCriteria);
   }
   if (confirmUpperCase === true) {
     selectedCriteria.push(upperArr);
-    console.log(selectedCriteria);
   }
   if (confirmLowerCase === true) {
     selectedCriteria.push(lowerArr);
-    console.log(selectedCriteria);
   }
   if (confirmSpecialChar === true) {
     selectedCriteria.push(specialArr);
-    console.log(selectedCriteria);
   }
-  // GENERATE PASSWORD FUNCTION
+
+  // Created a random generating password function that will take in the users selectedCriteria and passwordLength
   var generatePassword = function () {
+    // Created a variable that will collect the conversion of selectedCriteria (array) to a characters string.
+    //                               returns a string from an object
+    //                                           replaces all ("target","replacement")
     var characters = selectedCriteria.toString().replaceAll(",", "");
-    console.log(selectedCriteria);
+    // Created an empty varible to store the generatedPassword from generator.
     var generatedPassword = [];
+    // Created a for loop to iterate over the characters in the string.
+    //    intilize      condtion        increment
     for (var i = 0; i < passwordLength; i++) {
+      // Created a variable to store the randomGenerator function
       var randomGenerator =
+        // Character string
+        //        returns largest integer less than or equal to a given number
+        //                    returns floating-point pseudo number greater than or equal to 0 and less than 1
+        //                                  multply by characters length
         characters[Math.floor(Math.random() * characters.length)];
       generatedPassword.push(randomGenerator);
     }
-    console.log(generatedPassword);
+    // Add event listener to generate button and writePassword
+    generateBtn.addEventListener("click", writePassword);
+    // Created a function that will display the generated password
+    var writePassword = function () {
+      // Created a variable to store generatedPassword
+      var password = generatedPassword;
+      // Created a variable to store the reference to the textarea element
+      var passwordText = document.querySelector("#password");
+      // Setting the textarea to the value of the password.
+      passwordText.value = password;
+    };
+    writePassword();
   };
   generatePassword();
 }
-// console.log(selectedCriteria);
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// collectingCriteria
-
-// Created an empy array to collect selectedCriteria
