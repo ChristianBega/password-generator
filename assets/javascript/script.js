5ee598883e4eb9079ea9e1e548983ebca2ff368b
Chris B <cbega1227@gmail.com>
2022-10-29 20:51:27 -0700

    Completed password generator function. Still need to display to textarea.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -56,88 +56,82 @@ var lowerArr = [
   "z",
 ];
 var specialArr = ["!", "@", "#", "$", "*", "%"];
-// Set an empty array for selected criteria
+
+const selectedCriteria = [];
+
 // Collecting password criteria function
-var selectedCriteria = [];
-// Collecting password length from user.
-var userLengthInput = undefined;
-
-var collectCriteria = function () {
-  // Created variable called generateStart
-  var generateStart = window.confirm("Would you like to generate a password?");
-  // If gernerateStart is false alter user and exit function
-  if (generateStart === false) {
-    window.alert("Okay, goodbye.");
-    return;
-    // Else prompt user for password length
-  } else {
-    userLengthInput = prompt(
-      "Choose your password lenght (min: 8 || max: 128): "
-    );
-    // If criteria.length less than 8 or greater than 128 alter user, and retry.
-    if (userLengthInput < 8 || userLengthInput > 128) {
-      window.alert("Invalid password length.");
-      var reStart = confirm("Would you like to retry?");
-      if (reStart === true) {
-        collectCriteria();
-        return;
-      }
-    }
-
-    // Collecting rest of criteria from user using true/false
-    var confirmNumbers = confirm("Would you like numbers?");
-    var confirmUpperCase = confirm("Would you like upper case letters?");
-    var confirmLowerCase = confirm("Would you like lower case letters?");
-    var confirmSpecialChar = confirm("Would you like special characters?");
-    // Verifying user selected atleast one criteria
-    if (
-      confirmNumbers === false &&
-      confirmUpperCase === false &&
-      confirmLowerCase === false &&
-      confirmSpecialChar === false
-    ) {
-      var verification = window.confirm(
-        "You've selected ZERO character settings. Try again?"
-      );
-      if (verification === true) {
-        collectCriteria();
-        return;
-      } else {
-        window.alert("Okay, goodbye.");
-      }
-    }
-    // Sorting criteria if true then push to selectedCriteria Array
-    if (confirmNumbers === true) {
-      selectedCriteria.push(numbersArr.toString());
-    }
-    if (confirmUpperCase === true) {
-      selectedCriteria.push(upperArr.toString());
-    }
-    if (confirmLowerCase === true) {
-      selectedCriteria.push(lowerArr.toString());
-    }
-    if (confirmSpecialChar === true) {
-      selectedCriteria.push(specialArr.toString());
+function collectingCriteria() {
+  // Created var to collect the passwordLength input from user.
+  var passwordLength = prompt(
+    "Choose your password lenght (min: 8 || max: 128): "
+  );
+  // Created an if statment to verify passwordLength input is no less than 8 or no greater than 128
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Invalid password length.");
+    // Prompt user to retry if true recall collectingCriteria || EXIT
+    var reStart = confirm("Would you like to retry?");
+    if (reStart === true) {
+      collectingCriteria();
+      return;
     }
   }
-};
-// Calling collectCriteria function
-collectCriteria();
-console.log(selectedCriteria);
+  // Collecting rest of criteria from user using true/false
+  var confirmNumbers = confirm("Would you like numbers?");
+  var confirmUpperCase = confirm("Would you like upper case letters?");
+  var confirmLowerCase = confirm("Would you like lower case letters?");
+  var confirmSpecialChar = confirm("Would you like special characters?");
 
-// Creating function to randomly select a char from the selectedCriteria array.
-var randomCharacter = function (selectedCriteria) {
-  return selectedCriteria[Math.floor(Math.random() * selectedCriteria)];
+  // Verifying user selected atleast one criteria
+  if (
+    confirmNumbers === false &&
+    confirmUpperCase === false &&
+    confirmLowerCase === false &&
+    confirmSpecialChar === false
+  ) {
+    var verification = window.confirm(
+      "You've selected ZERO character settings. Try again?"
+    );
+    if (verification === true) {
+      collectingCriteria();
+      return;
+    } else {
+      window.alert("Okay, goodbye.");
+    }
+  }
+  // Sorting Criteria if true then push to selectedCriteria Array
 
-};
-randomCharacter();
-
-var generatePassword = function () {
-  var generatedPassword = "";
-};
-generatePassword();
-
-// Creating variable to hold users selected criteria
+  // Set an empty array for selected criteria
+  if (confirmNumbers === true) {
+    selectedCriteria.push(numbersArr);
+    console.log(selectedCriteria);
+  }
+  if (confirmUpperCase === true) {
+    selectedCriteria.push(upperArr);
+    console.log(selectedCriteria);
+  }
+  if (confirmLowerCase === true) {
+    selectedCriteria.push(lowerArr);
+    console.log(selectedCriteria);
+  }
+  if (confirmSpecialChar === true) {
+    selectedCriteria.push(specialArr);
+    console.log(selectedCriteria);
+  }
+  // GENERATE PASSWORD FUNCTION
+  var generatePassword = function () {
+    var characters = selectedCriteria.toString().replaceAll(",", "");
+    console.log(selectedCriteria);
+    var generatedPassword = [];
+    for (var i = 0; i < passwordLength; i++) {
+      var randomGenerator =
+        characters[Math.floor(Math.random() * characters.length)];
+      generatedPassword.push(randomGenerator);
+    }
+    console.log(generatedPassword);
+  };
+  generatePassword();
+}
+// console.log(selectedCriteria);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -151,3 +145,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// collectingCriteria
+
+// Created an empy array to collect selectedCriteria
